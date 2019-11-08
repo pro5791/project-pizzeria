baa716d8942ab7f4b870e0678006c336b2ca51b3
Przemek Rogala <przemek@rogala.priv.pl>
2019-11-08 20:30:29 +0100

    Added code to script.js - renderInMenu

--- src/js/script.js
+++ src/js/script.js
@@ -57,8 +57,15 @@
       const thisProduct = this;
       thisProduct.id = id;
       thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct)
     }
+    renderInMenu(){
+      const thisProduct = this;
+    }
+
   }
 
   const app = {
