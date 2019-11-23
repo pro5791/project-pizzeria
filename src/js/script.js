b9618662d531c3bc5e6ea111eebe003b124b4d15
Przemek Rogala <pro5791@mail.com>
2019-11-23 16:22:57 +0100

    Changed code in script.js - Cart Sum - v.6

--- src/js/script.js
+++ src/js/script.js
@@ -346,7 +346,9 @@
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated', {
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
   }
@@ -387,6 +389,10 @@
 
       thisCart.dom.toggleTrigger.addEventListener('click', function(){
         thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+      thisCart.dom.productList.addEventListener('updated', function(){
+        thisCart.update();
       });
     }
 
