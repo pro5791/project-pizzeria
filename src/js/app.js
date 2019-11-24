bfb0e100068ea037d85232f10a0961bc20cbb0fc
Przemek Rogala <pro5791@mail.com>
2019-11-24 20:05:42 +0100

    Changed code - taks 10.2 - v.4

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,18 @@ const app = {
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
     thisApp.activatePage(thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+      });
+    }
   },
 
   activatePage: function(pageID){
