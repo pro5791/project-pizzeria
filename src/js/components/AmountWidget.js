6c61f7de67c04bd9b2af50bbf51b72a49829727a
Przemek Rogala <pro5791@mail.com>
2019-11-25 18:51:47 +0100

    Changed code - task 10.3 - started

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,24 +1,24 @@
 /* eslint-disable linebreak-style */
 
 import {settings, select} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-class AmountWidget{
+class AmountWidget extends BaseWidget{
   constructor(element){
+    super(element, settings.amountWidget.defaultValue);
     const thisWidget = this;
 
     thisWidget.getElements(element);
-    thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(thisWidget.input.value);
+
     thisWidget.initActions();
 
     //console.log('AmountWidget: ', thisWidget);
     //console.log('constructor arguments: ', element);
   }
 
-  getElements(element){
+  getElements(){
     const thisWidget = this;
 
-    thisWidget.element = element;
     thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
     thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
     thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
