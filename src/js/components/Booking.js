53c2c03c5d7d8b4f0d10e8c89d32a7d232cc5499
Przemek Rogala <pro5791@mail.com>
2019-12-11 14:22:55 +0100

    Changed code - task 11.1 - v.8

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -77,11 +77,25 @@ class Booking{
 
     thisBooking.booked = {};
 
+    for(let item of bookings){
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+    }
+
     for(let item of eventsCurrent){
       thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
 
-    console.log('thisBooking.booked: ', thisBooking.booked);
+    const minDate = thisBooking.datePicker.minDate;
+    const maxDate = thisBooking.datePicker.maxDate;
+
+    for(let item of eventsRepeat){
+      if(item.repeat == 'daily'){
+        for(let loopDate = minDate; loopDate <= maxDate; loopDate = utils.addDays(loopDate, 1)){
+          thisBooking.makeBooked(utils.dateToStr(loopDate), item.hour, item.duration, item.table);
+        }
+      }
+    }
+    // console.log('thisBooking.booked: ', thisBooking.booked);
   }
 
   makeBooked(date, hour, duration, table) {
@@ -103,6 +117,8 @@ class Booking{
       thisBooking.booked[date][hourBlock].push(table);
     }
   }
+
+
 
   render(element){
     const thisBooking = this;
