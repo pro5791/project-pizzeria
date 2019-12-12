d52bc6926e8edd355b104200690ba5da2c04a7df
Przemek Rogala <pro5791@mail.com>
2019-12-12 11:21:32 +0100

    Changed code - task 11.1 - v.12

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -166,7 +166,7 @@ class Booking{
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
 
-    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelector(select.booking.tables);
+    thisBooking.dom.tables = thisBooking.dom.wrapper.querySelectorAll(select.booking.tables);
   }
 
   initWidgets(){
